a09c176f06c918377939c1375974707326a21961
tphamModLab <tmpham1@uci.edu>
2024-01-15 22:07:05 -0800

    feat(update): removed and cleaned up shopfiy configurations

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,4 @@
 
 module.exports = {
     // ...
-    'shopify-fields': {
-      enabled: true,
-      config: {
-        apiVersion: '2022-07', // Or one of Shopify's supported API versions
-        accessToken: process.env.SHOPIFY_API_SECRET, // The environment variable containing your private app's access token
-        shopName: process.env.HOST_NAME, // The environment variable containing your myshopify.com domain
-      },
-    },
   };
